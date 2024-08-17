7905af14a35e7f84d0df823d6ffc70be3023d4f4
InigmaEffect <inigmaeffect@gmail.com>
2024-08-17 16:04:35 +0500

    Delete reserve.php

--- reserve.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    // Save $email to the database or send it to your email, under the category "Reservers"
-    // Redirect to the SUPER FAN page
-    header("Location: superfans.html");
-    exit();
-}
-?>
